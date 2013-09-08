19d584353ac081d0984272fe0f05d274477c7073
Alexander Weld/manila <weldale@gmail.com>
2013-09-08 17:40:31 +0200

    ssp initial

---
+++ ssp.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+
+import sqlite3
+import fileinput
+import sys
+import cStringIO
+import re
+
+from optparse import OptionParser
+
+class SSP:
+	
+	DB_LOCATION = ":memory:"
+	DEFAULT_TABLE_NAME = 'data'
+
+	def __init__(self, **config):
+		self.config = config
+		self.conn = sqlite3.connect(SSP.DB_LOCATION)
+		self.cursor = self.conn.cursor()
+
+	def define_columns(self, columns):
+		self.columns = columns
+		self.column_line = ','.join([desc for desc,_ in columns])
+		
+		output = cStringIO.StringIO()
+		output.write('CREATE TABLE ')
+		output.write(self.getTableName())
+		output.write(' (\n')
+
+		columnStatements = ',\n'.join(['\t{0} {1}'.format(k,v) for k,v in columns])	
+
+		output.write(columnStatements)
+
+		output.write(')')
+
+		self.cursor.execute(output.getvalue())
+		self.conn.commit()
+
+	def getConfig(self, config_value, default = None):
+		return self.config.get(config_value) or default
+
+	def getTableName(self):
+		return self.getConfig('table_name', SSP.DEFAULT_TABLE_NAME)
+
+	def insert_data(self, data):
+	
+		#print self.column_line
+		#print data
+
+		output = cStringIO.StringIO()
+
+		output.write('INSERT INTO ')
+		output.write(self.getTableName())
+		output.write('(')
+		output.write( self.column_line )
+		output.write(') VALUES (')
+		output.write(','.join(['?'] * len(self.columns)))
+		output.write(')')
+
+		insert_statement = output.getvalue()
+
+		self.cursor.execute(insert_statement, tuple(data))
+
+	def onLineError(self, text):
+		if self.getConfig('ignore_wrong_lines', default = True):
+			sys.stderr.write(text)
+		else:
+			raise Exception(text)
+
+	def process(self, files):
+		
+		firstlinecolumns = self.getConfig('first_line_headers', default = False)
+		delimiter = self.getConfig('delimiter', default = ' ')
+
+		line_counter = 0
+
+		for line in fileinput.input(files):
+			line_counter += 1
+
+			line = line.strip()
+			split_line = re.split(delimiter, line)
+			
+			if firstlinecolumns:
+				firstlinecolumns = False
+				column_defs = [(k,'TEXT') for k in split_line]
+				self.define_columns(column_defs)
+				continue
+
+			split_line_len = len(split_line)
+			columns_len = len(self.columns)
+
+			data = split_line
+
+			insert = True
+
+			if split_line_len > columns_len:
+				if self.getConfig('join_long_rows', default = True):
+					last_data = ' '.join(data[(columns_len-1):])
+					data[(columns_len-1):] = []
+					data.append(last_data)
+				else:
+					self.onLineError('Line {0} too long ({1}/{2})'.format(line_counter, split_line_len, columns_len))
+					insert = False
+
+
+
+			if split_line_len < columns_len:
+
+				if self.getConfig('fill_short_rows', default = False):
+					data[split_line_len:] = ([''] * (columns_len - split_line_len))
+				else:
+					self.onLineError('Line {0} too short ({1}/{2})'.format(line_counter, split_line_len, columns_len))
+					insert = False
+
+
+			if insert:
+				self.insert_data(data)
+
+
+		self.conn.commit()
+
+	def dumpDB(self):
+		print self.column_line
+		self.cursor.execute('SELECT * FROM {0}'.format(self.getTableName()))
+		for row in self.cursor:
+			for field in row:
+				print field,
+			print ''
+
+	def execute(self, command):
+		self.cursor.execute(command)
+		for row in self.cursor:
+			for field in row:
+				print field,
+			print ''	
+
+
+parser = OptionParser()
+parser.add_option('-t', '--table', dest = 'table_name', metavar='TABLE_NAME', default=SSP.DEFAULT_TABLE_NAME)
+parser.add_option('-i', '--ignore-wrong-lines', dest = 'ignore_wrong_lines',  default=True)
+parser.add_option('-1', '--first-line-headers', dest = 'first_line_headers', default=True)
+parser.add_option('-j', '--join-long-rows', dest = 'join_long_rows', default=True)
+parser.add_option('-f', '--fill-short-rows', dest = 'fill_short_rows', default=False)
+parser.add_option('-d', '--delimiter', dest = 'delimiter', default=' ')
+
+(options, args) = parser.parse_args()
+
+options_dict = vars(options)
+
+print options_dict
+
+ssp = SSP( **options_dict )
+
+ssp.process(args[1:])
+
+ssp.execute(args[0])
